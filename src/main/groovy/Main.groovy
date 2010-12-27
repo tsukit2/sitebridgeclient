3483bbc34768d3d94534e7e9e430e84c66476def
tsukit <tsukit@3b91cfdc-d59a-2740-da8d-7c71c8cb03d3>
2010-12-27 00:41:38 +0000

    Make it works with most sites. No transformation yet and performance is poor

--- src/main/groovy/Main.groovy
+++ src/main/groovy/Main.groovy
@@ -11,11 +11,7 @@ import static groovyx.net.http.ContentType.*
 import static groovyx.net.http.ContentType.JSON as HTTPJSON
 import static groovyx.net.http.Method.*
 import java.util.zip.*
-
-// define where is the server
-//http = new HTTPBuilder('http://localhost:8080')
-http = new HTTPBuilder('http://sitebridgeserver.appspot.com')
-endpoint = new HTTPBuilder('http://adeethai.com')
+import org.apache.http.client.RedirectHandler
 
 // here is the main logic of the client and that's it
 reset()
@@ -25,60 +21,89 @@ while(true) {
 
    // if request found, satisfy it
    if (request) {
-      println request
-      response = fetchResponse(request)
-      satisfy([responseIndex:request.requestIndex, responseDetails:response])
-   
+      Thread.start {
+         println request
+         def response = fetchResponse(request)
+         println "${response.status}\n${response.headers}"
+         satisfy([responseIndex:request.requestIndex, responseDetails:response])
+      }
    } else {
       // wait a little bit if no request is found
       Thread.currentThread().sleep(1000) 
    }
 }
 
+def connectToServer(closure) {
+   // define where is the server
+   //def http = new HTTPBuilder('http://localhost:8080')
+   def http = new HTTPBuilder('http://sitebridgeserver.appspot.com')
+   try {
+      closure(http)
+   } finally {
+      http.shutdown()
+   }
+}
+
+def connectToEndPoint(closure) {
+   def endpoint = new HTTPBuilder('http://www.boost.org')
+   endpoint.client.redirectHandler = [isRedirectRequested: { resp, ctx -> false }] as RedirectHandler
+   try {
+      closure(endpoint)
+   } finally {
+      endpoint.shutdown()
+   }
+}
+
 def reset() {
    print "Resetting server...."
-   http.request(GET,HTTPJSON) { req ->
-      uri.path = '/bridgeconsole/reset'
+   connectToServer { http ->
+      http.request(GET,HTTPJSON) { req ->
+         uri.path = '/bridgeconsole/reset'
 
-      response.success = { resp, json ->
-         if (!json.status) {
-            println 'failed'
-            throw new RuntimeException("server reset failed: " + json.status)
+         response.success = { resp, json ->
+            if (!json.status) {
+               println 'failed'
+               throw new RuntimeException("server reset failed: " + json.status)
+            }
+            println 'succeeded'
          }
-         println 'succeeded'
       }
-   }
+   } 
 }
 
 def query() {
    print "Querying server...."
-   http.request(GET,HTTPJSON) { req ->
-      uri.path = '/bridgeconsole/query'
+   connectToServer { http ->
+      http.request(GET,HTTPJSON) { req ->
+         uri.path = '/bridgeconsole/query'
 
-      response.success = { resp, json ->
-         //print ".... ${json.request.getClass()} - ${json}...."
-         if (!(json.request instanceof JSONNull)) {
-            def requestObj = convertToMapAndArray(json.request)
-            println "found request ${requestObj.requestIndex}"
-            return requestObj
+         response.success = { resp, json ->
+            //print ".... ${json.request.getClass()} - ${json}...."
+            if (!(json.request instanceof JSONNull)) {
+               def requestObj = convertToMapAndArray(json.request)
+               println "found request ${requestObj.requestIndex}"
+               return requestObj
+            }
+            println 'no pending request'
          }
-         println 'no pending request'
       }
    }
 }
 
 def satisfy(responseObj) {
    print "Satisyfing: ${responseObj.responseIndex}...."
-   http.request(POST,HTTPJSON) { req ->
-      uri.path = '/bridgeconsole/satisfy'
-      body = responseObj
+   connectToServer { http ->
+      http.request(POST,HTTPJSON) { req ->
+         uri.path = '/bridgeconsole/satisfy'
+         body = responseObj
 
-      response.success = { resp, json ->
-         if (!json.satisfied) {
-            println 'failed'
-            throw new RuntimeException("satisfied request ${responseObj.responseIndex} failed")
+         response.success = { resp, json ->
+            if (!json.satisfied) {
+               println 'failed'
+               throw new RuntimeException("satisfied request ${responseObj.responseIndex} failed")
+            }
+            println 'succeeded'
          }
-         println 'succeeded'
       }
    }
 }
@@ -99,54 +124,59 @@ private convertToMapAndArray(jsonObj) {
 
 def fetchResponse(request) {
    print "Fetch response...."
-   endpoint.request(groovyx.net.http.Method."${request.requestDetails.method}") { req ->
-      // set the path to match the math info
-      if (request.requestDetails.pathInfo) {
-         println "pathInfo = ${request.requestDetails.pathInfo}"
-         uri.path = request.requestDetails.pathInfo
-      }
-      
-      // set query string if any
-      if (request.requestDetails.query) {
-         uri.query = request.requestDetails.query
-      }
-
-      // pass on the original request's headers
-      headers.clear()
-      headers = request.requestDetails.headers.inject([:]) { m,e ->
-         if (e.key != 'Host') {
-            m[e.key] = e.value
+   connectToEndPoint { endpoint ->
+      endpoint.request(groovyx.net.http.Method."${request.requestDetails.method}") { req ->
+         // set the path to match the math info
+         if (request.requestDetails.pathInfo) {
+            println "pathInfo = ${request.requestDetails.pathInfo}"
+            uri.path = request.requestDetails.pathInfo
          }
-         return m
-      }
          
+         // set query string if any
+         if (request.requestDetails.query) {
+            uri.query = request.requestDetails.query
+         }
 
-      response.success = { resp ->
-         println 'succeeded'
-         
-         // prepare the body bytes to send. Rezip it if need to
-         def bytes = resp.entity.content.bytes
-         /*
-         if (resp.headers.'Content-Encoding'?.toLowerCase()?.contains('gzip')) {
-            def bytearray = new ByteArrayOutputStream()
-            def zip = new GZIPOutputStream(bytearray)
-            zip << bytes
-            zip.finish()
-            bytes = bytearray.toByteArray()
-         }*/
-         
-         // finally return the result
-         return [status:resp.status,
-                 headers:resp.headers.inject([:]) { m,h -> 
-                    if (h.name != 'Content-Encoding') {
-                       m[h.name] = h.value;
-                    } 
-                    return m 
-                 },
-                 bodyBytes:bytes]
-         
+         // pass on the original request's headers
+         headers.clear()
+         headers = request.requestDetails.headers.inject([:]) { m,e ->
+            if (e.key != 'Host') {
+               m[e.key] = e.value
+            }
+            return m
+         }
+            
+         // create response handler closure and configure for both success and failure
+         def responseHandler = { resp ->
+            println 'Ok'
+            // prepare the body bytes to send. Rezip it if need to
+            def bytes = resp.entity.content.bytes
+            println "***** ${bytes.size()}"
+            /*
+            if (resp.headers.'Content-Encoding'?.toLowerCase()?.contains('gzip')) {
+               def bytearray = new ByteArrayOutputStream()
+               def zip = new GZIPOutputStream(bytearray)
+               zip << bytes
+               zip.finish()
+               bytes = bytearray.toByteArray()
+            }*/
+            
+            // finally return the result
+            return [status:resp.status,
+                    headers:resp.headers.inject([:]) { m,h -> 
+                       def val = h.value
+                       if (h.name == 'Content-Length' && (val as long) != bytes.size()) {
+                          val = bytes.size().toString()
+                       }
+                       m[h.name] = val;
+                       return m 
+                    },
+                    bodyBytes:bytes]
+            
+         }
+         response.success = responseHandler
+         response.failure = responseHandler
       }
    }
 }
-
 

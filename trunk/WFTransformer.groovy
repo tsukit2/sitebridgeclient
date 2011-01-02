aefa550801b48846778fc7e78d668e7afa97cff1
tsukit <tsukit@3b91cfdc-d59a-2740-da8d-7c71c8cb03d3>
2011-01-02 11:09:51 +0000

    make it work with wf.com but still some bugs

--- trunk/WFTransformer.groovy
+++ trunk/WFTransformer.groovy
@@ -8,13 +8,31 @@ onRequest = {
 }
 
 onResponse = { 
-   // replace cookie's domain
+   // transform cookie
    if (response.headers['Set-Cookie']) {
-
-
-
+      def cookieValue = response.headers['Set-Cookie']
+      response.headers['Set-Cookie'] = (cookieValue instanceof List 
+         ? cookieValue.collect { transformCookie(it) }
+         : transformCookie(cookieValue))
    }
 
 }
+
+transformCookie = { cookie ->
+   switch(cookie) {
+      case ~/MBASESSIONID.*/:
+         return (server.protocol.toLowerCase() == 'https' 
+            ? cookie
+            : cookie.split(';').findAll { it.trim() != 'secure' }.join(';') + ';')
+
+      case ~/mba_cookie_prod.*/:
+         return cookie.split(';').findAll { !it.trim().startsWith('domain') }.join(';') + ';'
+         //return cookie.replaceAll(/domain=\S*/, "domain=.${server.host}") 
+
+      default:
+         return cookie
+   }
+}
+
 
 

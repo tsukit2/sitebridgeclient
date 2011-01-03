34c088be56a2c949367c38e2befde18f08c3084f
tsukit <tsukit@3b91cfdc-d59a-2740-da8d-7c71c8cb03d3>
2011-01-03 06:29:29 +0000

    complete refactor. Need to test more

--- trunk/src/main/groovy/Main.groovy
+++ trunk/src/main/groovy/Main.groovy
@@ -1,287 +1,69 @@
 @Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.5.1' )
 @Grab(group='net.sf.json-lib', module='json-lib', version='2.3', classifier='jdk15' )
+@Grab(group='log4j', module='log4j', version='1.2.16')
+import com.eddy.sitebridgeclient.*
+import org.apache.log4j.*
 
-import net.sf.json.*
-import groovyx.net.http.*
-import static groovyx.net.http.ContentType.*
-import static groovyx.net.http.ContentType.JSON as HTTPJSON
-import static groovyx.net.http.Method.*
-import java.util.zip.*
-import org.apache.http.client.RedirectHandler
-import org.apache.http.message.* 
-import java.util.concurrent.*
+// configure log4j
+PropertyConfigurator.configure('log4j.properties');
 
-serverURL = 'http://localhost:8080'
-//serverURL = 'http://sitebridgeserver.appspot.com'
-endpointURL = args.size() ? args[0] : 'http://www2.research.att.com/~bs'
-
-
-transformers = loadTransformers([
-   'BasicTransformer.groovy',
-   'WFTransformer.groovy',
-   //'C:/Personal/development/gaelyk/sitebridgeclient/BjarneTransformer.groovy'
-   //'/Users/eddy/Development/googleapp/sitebridgeclient/BasicTransformer.groovy',
-   //'/Users/eddy/Development/googleapp/sitebridgeclient/BjarneTransformer.groovy'
-   ],
-   [bridge:[serverURL:serverURL, endpointURL:endpointURL]])
-
-// thread pool to execute anything asynchronously
-executor = Executors.newCachedThreadPool() //newFixedThreadPool(60)
-
-try { 
-   doit()
-} finally {
-   executor.shutdown()
+// show usage if the parameters are not given
+if (args.size() == 0) {
+   println "Main Usage:"
+   println "   <endpointURL> <serverURL> [<transformation script>*]"
+   println "Alternative:"
+   println "   warmup <serverURL>"
+   println "   report [<# requests to go back - default 30>]"
+   System.exit(1)
 }
 
-def doit() {
-   // here is the main logic of the client and that's it
-   reset()
-   while(true) {
-      // query for the requests. this return list of requests which could be empty
-      def requests = query(); 
+// now determine what to do
+switch (args[0]) {
+   case 'warmup': warmup(); break
+   case 'report': report(); break
+   default      : bridge()
+}
 
-      // if request found, satisfy it
-      if (requests) {
-         executor.submit( {
-            // get responses asynchronously
-            def responses = requests.collect { request ->
-               executor.submit({
-                  try {
-                     // delegate for handlers
-                     def delegate = [
-                        bridge:[serverURL:serverURL, endpointURL:endpointURL],
-                        request:request.requestDetails
-                        ]
-
-                     // get onRequest handlers and call it
-                     def onRequestHandlers = transformers*.onRequest*.clone()
-                     onRequestHandlers*.delegate = delegate
-                     onRequestHandlers*.call()
-
-                     println request
-
-                     // fetch the response and add to the delegate
-                     def response = fetchResponse(request)
-                     delegate.response = response
-
-                     println "${response.status}\n${response.headers}"
-
-                     // get onResponse handlers and call it
-                     def onResponseHandlers = transformers*.onResponse*.clone()
-                     onResponseHandlers*.delegate = delegate
-                     onResponseHandlers*.call()
-
-                     // return the response
-                     return [responseIndex:request.requestIndex, responseDetails:response]
-                  } catch(ex) { ex.printStackTrace() }
-               } as Callable<Object>)
-            }
-
-            // get all the results (wait for it) and satisfy it
-            satisfy(responses.collect { it.get() })
-         } as Runnable)
-      } else {
-         // wait a little bit if no request is found
-         Thread.currentThread().sleep(1000) 
-      }
+def warmup() {
+   // check parameters
+   if (args.size() <= 2) {
+      println "ERROR: Please specify the URL for the bridge site"
+      System.exit(1)
    }
+
+   // if reach here, treat the second parameter as the url
+   // and start the warmup
+   def serverURL = args[1]
+   def controller = new Controller(serverURL)
+   println "Warming up server..."
+   controller.startWarmingUp()
+
+   // register the shutdown hook up to finish the controller
+   Runtime.runtime.addShutdownHook(
+      new Thread({ controller.finish() } as Runnable))
 }
 
-def loadTransformers(files, initBindingMap) {
-   def shell = new GroovyShell()
-   def scripts = files.collect { shell.parse(it as File) }
-   scripts.each { it.binding = new Binding(initBindingMap.clone()) }
-   scripts*.run()
-   return scripts*.binding
+def report() {
+   println "Report"
+
 }
 
-def createHTTPBuilder(url) {
-   def http = new HTTPBuilder(url)
-   if (!(url =~ /localhost/) && System.properties.'http.proxyHost') {
-      http.setProxy(System.properties.'http.proxyHost', 
-                    System.properties.'http.proxyPort' as int, 
-                    'http')
+def bridge() {
+   // check parameters
+   if (args.size() < 2) {
+      println "ERROR: Please specify at least the URL of both endpoint and bridge site"
+      System.exit(1)
    }
-   return http
-}
 
-def connectToServer(closure) {
-   // define where is the server
-   def http = createHTTPBuilder(serverURL)
-   try {
-      closure(http)
-   } finally {
-      http.shutdown()
-   }
-}
+   // if reach here, extract the parameters and start bridging
+   def endpointURL = args[0]
+   def serverURL = args[1]
+   def transformerScripts = args[2..<args.size()]
+   def controller = new Controller(serverURL, endpointURL, transformerScripts, './data')
+   println "Bridging..."
+   controller.startBridging()
 
-def connectToEndPoint(closure) {
-   def endpoint = createHTTPBuilder(endpointURL)
-   endpoint.client.redirectHandler = [isRedirectRequested: { resp, ctx -> false }] as RedirectHandler
-   try {
-      closure(endpoint)
-   } finally {
-      endpoint.shutdown()
-   }
-}
-
-def reset() {
-   print "Resetting server...."
-   1.downto(0) { count ->
-      connectToServer { http ->
-         http.request(GET,HTTPJSON) { req ->
-            uri.path = '/bridgeconsole/reset'
-
-            response.success = { resp, json ->
-               if (!json.status) {
-                  println 'failed'
-                  throw new RuntimeException("server reset failed: " + json.status)
-               }
-               print "...${count}"
-            }
-         }
-      } 
-   }
-   println '...succeeded'
-}
-
-def query() {
-   print "Querying server...."
-   connectToServer { http ->
-      http.request(GET,HTTPJSON) { req ->
-         uri.path = '/bridgeconsole/query'
-
-         response.success = { resp, json ->
-            //print ".... ${json.request.getClass()} - ${json}...."
-            def requests = convertToMapAndArray(
-               JSONArray.fromObject(inflateByteArrayToObj(convertToMapAndArray(json.payload) as byte[])))
-            if (requests) {
-               println "found ${requests.size()} request(s)"
-               return requests
-            }
-            println 'no pending request'
-         }
-      }
-   }
-}
-
-def satisfy(responses) {
-   print "Satisyfing: ${responses*.responseIndex}...."
-   connectToServer { http ->
-      http.request(POST,HTTPJSON) { req ->
-         uri.path = '/bridgeconsole/satisfy'
-         body = [payload:deflateObjectToByteArray(
-                           JSONArray.fromObject(responses).toString())]
-
-         response.success = { resp, json ->
-            if (!json.satisfied) {
-               println 'failed'
-               throw new RuntimeException("satisfied request ${responses*.responseIndex} failed")
-            }
-            println 'succeeded'
-         }
-      }
-   }
-}
-
-private deflateObjectToByteArray(obj) {
-   def bytes = new ByteArrayOutputStream()
-   def outstream = new ObjectOutputStream(new GZIPOutputStream(bytes))
-   outstream.writeObject(obj)
-   outstream.close()
-   bytes.toByteArray()
-}
-
-private inflateByteArrayToObj(bytearray) {
-   return new ObjectInputStream(
-      new GZIPInputStream(new ByteArrayInputStream(bytearray))).readObject()
-
-}
-
-// utility method to convert json object to map and array
-private convertToMapAndArray(jsonObj) {
-  switch(jsonObj) {
-     case List: 
-        return jsonObj.inject([]) { l, elem -> l << convertToMapAndArray(elem); l }
-     case Map:  
-        return jsonObj.inject([:]) { m, entry -> m[entry.key] = convertToMapAndArray(entry.value); m }
-     case JSONNull:
-        return null
-     default:   
-        return jsonObj
-  }
-}
-
-def fetchResponse(requestObj) {
-   print "Fetch response...."
-   connectToEndPoint { endpoint ->
-      endpoint.request(groovyx.net.http.Method."${requestObj.requestDetails.method}") { req ->
-         // set the path to match the math info
-         if (requestObj.requestDetails.pathInfo) {
-            println "pathInfo = ${requestObj.requestDetails.pathInfo}"
-            uri.path = requestObj.requestDetails.pathInfo
-         }
-         
-         // set query string if any
-         if (requestObj.requestDetails.query) {
-            uri.query = requestObj.requestDetails.query
-         }
-
-         // set body if params are given
-         if (requestObj.requestDetails.params) {
-            /*
-            // we need to convert to empty string because of the bug in httpbuilder
-            def nullToEmptyStrMap = new HashMap(requestObj.requestDetails.params)
-            nullToEmptyStrMap.each { e -> if (e.value == null) e.value = '' }
-            */
-            requestContentType = URLENC 
-            body = requestObj.requestDetails.params
-         }
-
-         // pass on the original request's headers. We need to merge the value first
-         // because headers can be duplicate and HttpBuilder doesn't support
-         // that notion
-         /*
-         headers.clear()
-         headers = requestObj.requestDetails.headers.inject([:] { m,e ->
-            m[e.key] = e.value.join(';') 
-            return m
-         }
-         */
-         headers.clear()
-         requestObj.requestDetails.headers.each { k,v ->
-            if (k != 'Content-Length') {
-               if (v instanceof List) {
-                  v.each { request.addHeader(new BasicHeader(k, it)) }
-               } else {
-                  request.addHeader(new BasicHeader(k, v))
-               }
-            }
-         }
-
-         // create response handler closure and configure for both success and failure
-         def responseHandler = { resp ->
-            // finally return the result
-            println 'Ok'
-            def bytes = resp.entity.content.bytes
-            return [status:resp.status,
-                    headers:resp.headers.inject([:]) { m,h -> 
-                       def val = h.value
-                       if (h.name == 'Content-Length' && (val as long) != bytes.size()) {
-                          val = bytes.size().toString()
-                       }
-                       def existingValue = m[h.name]
-                       m[h.name] = (existingValue != null 
-                          ? (m[h.name] instanceof List ? existingValue << val : [existingValue, val])
-                          : val)
-                       return m 
-                    },
-                    bodyBytes:bytes]
-            
-         }
-         response.success = responseHandler
-         response.failure = responseHandler
-      }
-   }
+   // register the shutdown hook up to finish the controller
+   Runtime.runtime.addShutdownHook(
+      new Thread({ controller.finish() } as Runnable))
 }
---
+++ trunk/src/main/groovy/com/eddy/sitebridgeclient/Transformers.groovy
@@ -0,0 +1,95 @@
+package com.eddy.sitebridgeclient
+
+import org.apache.log4j.*
+
+/**
+ * Facilitate the tranformation process of the bridging. This site works closely with Bridge
+ * to drive transformers through the request and response. It intimately knows how the 
+ * resquest and response are structured though the outside world does not necessarily need
+ * to.
+ */
+class Transformers {
+   private static Logger log = Logger.getLogger(this.name)
+   private transformers
+   private serverURL, endpointURL
+
+   /**
+    * Constructor.
+    *
+    * @param scriptNames            Names of the transformer script.
+    * @param serverURL           URL of the sitebridge server.
+    * @param endpointURL         URL of the endpoint site to be bridged.
+    */
+   Transformers(List<String> scriptNames, String serverURL, String endpointURL) {
+      // save off the two URL for later use
+      this.serverURL = serverURL
+      this.endpointURL = endpointURL
+
+      // load up the transformers for later use
+      transformers = loadTransformers(scriptNames, 
+         [bridge:[serverURL:serverURL, endpointURL:endpointURL]])
+   }
+
+   /**
+    * Transform request as given in the map.
+    * 
+    * @param request             Request object to be transformed.
+    *
+    * @return a map representing the transformation of the request. This object
+    *    will be needed in transformResponse method.
+    */
+   Map transformRequest(Map request) {
+      // set up delegate map
+      def delegate = [
+         bridge:[serverURL:serverURL, endpointURL:endpointURL],
+         request:request.requestDetails
+      ]
+
+      // log request before transformation
+      log.debug "Request before transformation: ${request.requestDetails}"
+
+      // get onRequest handlers and call it
+      def onRequestHandlers = transformers*.onRequest*.clone()
+      onRequestHandlers*.delegate = delegate
+      onRequestHandlers*.call()
+      log.info "Finish transforming request"
+
+      // log request after transformation
+      log.debug "Request after transformation: ${request.requestDetails}"
+
+      // return the delegate used here as the context
+      return delegate
+   }
+
+   /**
+    * Transform response as given in the map.
+    *
+    * @param response            Response object to be transformed.
+    */
+   void transformResponse(Map requestContext, Map response) {
+      // set up delegate map
+      def delegate = requestContext
+      delegate.response = response.responseDetails
+
+      // log response before transformation
+      log.debug "Response before transformation: ${response.requestDetails}"
+
+      // get onResponse handlers and call it
+      def onResponseHandlers = transformers*.onResponse*.clone()
+      onResponseHandlers*.delegate = delegate
+      onResponseHandlers*.call()
+      log.info "Finish transforming response"
+
+      // log response before transformation
+      log.debug "Response after transformation: ${response.requestDetails}"
+   }
+
+   private loadTransformers(scriptNames, initBindings) {
+      def shell = new GroovyShell()
+      def scripts = scriptNames.collect { shell.parse(it as File) }
+      scripts.each { it.binding = new Binding(initBindings.clone()) }
+      scripts*.run()
+      return scripts*.binding
+   }
+}
+

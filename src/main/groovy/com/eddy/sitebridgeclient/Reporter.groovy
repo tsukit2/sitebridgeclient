552d69d16480eba16d70fca9e44ac33791dcef98
tsukit <tsukit@3b91cfdc-d59a-2740-da8d-7c71c8cb03d3>
2011-01-04 05:10:32 +0000

    port to work on linux

---
+++ src/main/groovy/com/eddy/sitebridgeclient/Reporter.groovy
@@ -0,0 +1,17 @@
+package com.eddy.sitebridgeclient
+
+
+class Reporter {
+
+   private basedir
+
+   Reporter(String reportDir) {
+      this.baseDir = new File(reportDir)
+   }
+
+   
+
+
+   private 
+}
+
